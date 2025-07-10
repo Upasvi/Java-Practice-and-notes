e13d3388fd7e0f72563e1c980830405e3255c08b
Upasvi <upasviy@gmail.com>
2025-07-10 11:13:41 +0530

    day3

---
+++ video23.java
@@ -0,0 +1,29 @@
+public class video23 {
+    public static void main(String[] args){
+        for(int i=100; i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+// public class video23 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0; i<n;i++){
+//              System.out.println(2 *i+1);
+//         }
+//         System.out.println("reverse:");
+// /////    Reversing //////
+//         for(int i=5; i>0;i--){
+//             System.out.println(2*i-1);
+//         }
+//     }
+// }
---
+++ video24.java
@@ -0,0 +1,33 @@
+public class video24 {
+    public static void main(String[] args){
+        for( int i =0 ; i<5;i++){
+
+            if(i==2){
+            System.out.println("Ending the loop");
+          continue;
+        }///iske niche jo h usko chodo aur sidha agli iteration p move kro isliye continue ko upr likha
+
+            System.out.println(i);
+            System.out.println("this works");
+            
+        }
+        
+    }
+}
+
+
+
+/*public class video24 {
+    public static void main(String[] args){
+        for( int i =0 ; i<5;i++){
+            System.out.println(i);
+            System.out.println("this works");
+            if(i==2){
+            System.out.println("Ending the loop");
+            break;
+        }
+        }
+        
+    }
+}
+*/
