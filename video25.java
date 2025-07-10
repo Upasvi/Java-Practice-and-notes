19d15864ffb914ffccca46bb7ab41c4fc8de35a0
Upasvi <upasviy@gmail.com>
2025-07-10 11:54:54 +0530

    day3

---
+++ video25.java
@@ -0,0 +1,79 @@
+public class video25 {
+    public static void main(String[] args){
+        int sum =0;
+        int n = 10;
+        int mul = 1;
+        for(int i =0; i<=10;i++){
+            mul = 8*i;
+           sum = sum+mul;
+            System.out.println(mul);
+           
+        }
+         System.out.println(sum);
+    }
+}
+
+
+/*public class video25 {
+    public static void main(String[] args){
+int num =4;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}*/
+
+/*public class video25 {
+    public static void main(String[] args){
+
+        int n = 10;
+        for(int i = n;i>=0;i--){
+            int mul = 2*i;
+            System.err.println("10 x " + i + " = " + mul);
+        }
+    }
+}*/
+
+
+/*public class video25 {
+    public static void main(String[] args){
+        int n = 10;
+        for(int i =0; i<=10;i++){
+            int mul = 2*i;
+            System.out.print("2 x " + i + " = " );
+            System.out.println(mul);
+        }
+        
+    }
+}
+*/
+
+/*public class video25 {
+    public static void main(String[] args){
+        int n=10; //10 numer ka sum krege
+        int sum=0;
+        for(int i=0;i<n;i++){
+        sum +=2*i;
+           
+        }
+        System.out.println(sum);
+    }
+}
+ */
+
+/*public class video25 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=n;i>0;i--){
+            for(int j = 0; j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+*/
