c134c65780a3858361c1b96a49ba56c861071bb9
Upasvi <upasviy@gmail.com>
2025-07-11 12:13:25 +0530

    day4

---
+++ video31.java
@@ -0,0 +1,25 @@
+public class video31 {
+    static int logic(int x,int y){
+        int z;
+        if(x>y){
+            z=x+y;
+        }
+        else{
+            z=(x+y)*5;
+        }
+        return z;
+    }
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b= 6;
+        int c = logic(a,b);
+
+        int a1=7;
+        int b1 =8;
+        int c1 = logic(a1, b1);
+
+        System.out.println(c);
+        System.out.println(c1);
+    }        
+    }
